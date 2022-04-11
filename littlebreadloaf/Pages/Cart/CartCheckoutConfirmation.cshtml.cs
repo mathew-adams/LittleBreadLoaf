5da8b8332f4a81eb29a2c5e405594bfeebc6866c
Mathew Adams <Matadams@gmail.com>
2022-04-11 20:22:02 +1200

    Add view PDF function
    This is to account for the shear stupidity of my ASP hosting provider

--- littlebreadloaf/Pages/Cart/CartCheckoutConfirmation.cshtml.cs
+++ littlebreadloaf/Pages/Cart/CartCheckoutConfirmation.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using littlebreadloaf.Data;
 using Microsoft.EntityFrameworkCore;
+using SelectPdf;
+using System.IO;
 
 namespace littlebreadloaf.Pages.Cart
 {
@@ -52,6 +54,55 @@ namespace littlebreadloaf.Pages.Cart
             }
             
             return Page();
+        }
+        public async Task<IActionResult> OnGetPrintAsync ()
+        {
+
+            if (string.IsNullOrEmpty(ProductOrderID) || !Guid.TryParse(ProductOrderID, out Guid productOrderID))
+            {
+                return new RedirectToPageResult("/Cart/CartView");
+            }
+
+            ProductOrder = await _context.ProductOrder.FirstOrDefaultAsync(f => f.OrderID == productOrderID);
+            if (ProductOrder == null)
+            {
+                return new RedirectToPageResult("/Cart/CartView");
+            }
+
+            ViewData["HasAddress"] = ProductOrder.ContactAddress > 0;
+
+            if (ProductOrder.ContactAddress > 0)
+            {
+                NzAddressDeliverable = await _context.NzAddressDeliverable.FirstOrDefaultAsync(f => f.address_id == ProductOrder.ContactAddress);
+            }
+
+            var url = Url.Page("/Orders/InvoicePrint", new { ProductOrder.OrderID });
+
+            var absUrl = string.Format("{0}://{1}{2}", HttpContext.Request.Scheme, HttpContext.Request.Host, url);
+
+            HtmlToPdf converter = new SelectPdf.HtmlToPdf();
+
+            converter.Options.MarginBottom = 20;
+            converter.Options.MarginTop = 20;
+            converter.Options.MarginRight = 20;
+            converter.Options.MarginLeft = 20;
+            PdfDocument doc = converter.ConvertUrl(absUrl);
+
+            using (var msInvoice = new System.IO.MemoryStream())
+            {
+                doc.Save(msInvoice);
+                // close pdf document
+
+                doc.Close();
+                msInvoice.Position = 0;
+
+                string contentType = "application/pdf";
+               // string fileName = "sample.pdf";
+
+                var file = File(msInvoice.ToArray(), contentType);
+
+                return file;
+            }
         }
     }
 }
